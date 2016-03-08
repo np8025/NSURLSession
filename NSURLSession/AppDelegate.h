848765d6540d5828a8929c19f4eb830159fbf20b
HCL <HCL@dgs-macbook-air.local>
2016-03-08 11:04:38 +0800

    Initial Commit

---
+++ NSURLSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSession
+//
+//  Created by HCL on 16/3/8.
+//  Copyright © 2016年 胡成龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSession
+//
+//  Created by HCL on 16/3/8.
+//  Copyright © 2016年 胡成龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
