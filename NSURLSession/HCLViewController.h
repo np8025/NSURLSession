fc0a4afcee94b60ab49344d9d59384717fbf575f
HCL <HCL@HCLdeMac.local>
2016-03-15 17:36:06 +0800

    更新

---
+++ NSURLSession/HCLViewController.h
@@ -0,0 +1,13 @@
+//
+//  HCLViewController.h
+//  NSURLSession
+//
+//  Created by HCL on 16/3/15.
+//  Copyright © 2016年 胡成龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCLViewController : UIViewController
+
+@end
---
+++ NSURLSession/HCLViewController.m
@@ -0,0 +1,27 @@
+//
+//  HCLViewController.m
+//  NSURLSession
+//
+//  Created by HCL on 16/3/15.
+//  Copyright © 2016年 胡成龙. All rights reserved.
+//
+
+#import "HCLViewController.h"
+
+@interface HCLViewController ()
+
+@end
+
+@implementation HCLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"HCL");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
